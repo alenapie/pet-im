027b2e0c90f541c9c0cbb9c8a40ae5120ea9a8bb
alenapie <alenapie@yandex.ru>
2024-11-21 02:19:52 +0300

    add input

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -3,13 +3,14 @@ import styles from "./input.module.scss";
 import { FC, useState } from "react";
 
 type Props = {
-  color: "mutedPrimary" | "inputSpecial";
-  fixedSize?: "fixedSize";
+  //! todo: добавить className чтобы снаружи менять цвет
+  color: "mutedPrimary" | "inputSpecial"; //! todo: поправить цвета
+  fixedSize?: "fixedSize"; //! todo: развернуть логику и  переименовать в fullSize
   placeholder?: string;
 };
 
 export const Input: FC<Props> = ({
-  color = "mutedPrimary",
+  color = "mutedPrimary", //! todo: исправить на primary
   fixedSize,
   placeholder = "",
 }) => {
@@ -18,21 +19,24 @@ export const Input: FC<Props> = ({
   const isPlaceholderVisible = value === "" && !isFocused;
   return (
     <div className={styles.inputWrapper}>
+      {" "}
+      //! todo: ???
       <input
         className={clsx(
           styles.root,
           fixedSize && styles[fixedSize],
           styles[color]
         )}
-        type="text"
         value={value}
+        //! todo: убрать стейты и сделать проп onChange
         onChange={(e) => setValue(e.target.value)}
+        //! todo: убрать это все
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
       />
-      {isPlaceholderVisible && placeholder && (
-        <span className={styles.placeholder}>{placeholder}</span>
-      )}
+      {/* {isPlaceholderVisible && placeholder && (
+        <span className={styles.placeholder}>{placeholder}</span> //! todo: убрать, вынести в input
+      )} */}
     </div>
   );
 };
